b3b38b00fb799ff9e4d99eba11748a5963f82bf5
Jinkyoung Park <qwr7652@gmail.com>
2024-03-21 00:09:23 +0900

    Fix: 버그수정

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Box from "./component/Box";
 // 5. 3번 4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
 
 // 과제1 : 컴퓨터 결과 보이게 하기
-// 과제2. 6. 승패결과에 따라 테두리 색이 바뀐다. (이기면 )
+// 과제2. 6. 승패결과에 따라 테두리 색이 바뀐다. (이기면)
 
 const choice = {
   rock:{
@@ -30,16 +30,26 @@ function App() {
   const [userSelect,setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect]=useState(null);
   const [result,setResult]=useState("");
-  const play=(userChoice)=>{
+  const play=(userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
     setResult(judgement(choice[userChoice],computerChoice));
   };
 
+  const randomChoice=()=>{
+    let itemArray = Object.keys(choice);  //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    
+    return choice[final];
+  };
+  
   const judgement = (user, computer) => {
     console.log("user", user, "computer", computer);
-
+  
+    // 유저 입장에서
     if (user.name === computer.name) {
       return "tie";
     } else if (user.name === "Rock")
@@ -48,23 +58,11 @@ function App() {
       return computer.name == "Paper" ? "win" : "lose";
     else if (user.name === "Paper") return computer.name === "Rock" ? "win" : "lose";
   };
-
-  const randomChoice=()=>{
-
-    let itemArray = Object.keys(choice);  //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
-    console.log("item array", itemArray);
-
-    let randomItem = Math.floor(Math.random() * itemArray.length);
-    let final = itemArray[randomItem];
-
-    return choice[final];
-  }
-
   return (
     <div>
       <div className="main">
         <Box title="You" item={userSelect} result={result}/>
-        <Box title="Computer" item={computerSelect}/>
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
       <div className="main">
       <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const Box = (props) => {
-  console.log("props", props);
+const Box = (props) => {  
+  let result;
+  if (
+      props.title === "Computer" && 
+      props.result !== "tie" && 
+      props.result !== ""
+      ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
+  if (props.title === "Computer") {
+    console.log("computer", result);
+  }
+
   return (  
-        <div className="box">
+        <div className={`box ${result}`}>
         <h1>{props.title}</h1>
+        <h2 data-testid="item-name">{props.item && props.item.name}</h2>
         <img className="item-img" src={props.item && props.item.img} />
-        <h2>{props.result}</h2>
+        <h2>{result}</h2>
         </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box;
