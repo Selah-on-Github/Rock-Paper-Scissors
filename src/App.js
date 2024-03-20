e86aef74fc956e48c46e81e1b0937db06c9afcc6
Jinkyoung Park <qwr7652@gmail.com>
2024-03-21 01:12:44 +0900

    final commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import {useState} from "react";
 import './App.css';
 import Box from "./component/Box";
 
-// 1. 박스 2개 (타이틀, 사진, 결과)
-// 2. 가위 바위 보 버튼이 있다.
-// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-// 4. 컴퓨터는 랜덤하게 아이템 선택이 된다. 
-// 5. 3번 4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
-
-// 과제1 : 컴퓨터 결과 보이게 하기
-// 과제2. 6. 승패결과에 따라 테두리 색이 바뀐다. (이기면)
-
 const choice = {
   rock:{
     name:"Rock",
@@ -39,7 +30,7 @@ function App() {
 
   const randomChoice=()=>{
     let itemArray = Object.keys(choice);  //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
-    console.log("item array", itemArray);
+    // console.log("item array", itemArray);
     let randomItem = Math.floor(Math.random() * itemArray.length);
     let final = itemArray[randomItem];
     
@@ -47,8 +38,8 @@ function App() {
   };
   
   const judgement = (user, computer) => {
-    console.log("user", user, "computer", computer);
-  
+    // console.log("user", user, "computer", computer);
+
     // 유저 입장에서
     if (user.name === computer.name) {
       return "tie";
@@ -65,9 +56,9 @@ function App() {
         <Box title="Computer" item={computerSelect} result={result} />
       </div>
       <div className="main">
-      <button onClick={() => play("scissors")}>가위</button>
-      <button onClick={() => play("rock")}>바위</button>
-      <button onClick={() => play("paper")}>보</button>
+      <button className="scissors" onClick={() => play("scissors")}>가위</button>
+      <button className="rock" onClick={() => play("rock")}>바위</button>
+      <button className="paper" onClick={() => play("paper")}>보</button>
       </div>
     </div>
   );
