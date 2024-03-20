c09d6c3b50ba1c090033600ce309addda9b627b2
Jinkyoung Park <qwr7652@gmail.com>
2024-03-20 23:31:46 +0900

    Feat : 강의에서 배운 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Box from "./component/Box";
 // 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
 // 4. 컴퓨터는 랜덤하게 아이템 선택이 된다. 
 // 5. 3번 4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
-// 6. 승패결과에 따라 테두리 색이 바뀐다. (이기면 )
+
+// 과제1 : 컴퓨터 결과 보이게 하기
+// 과제2. 6. 승패결과에 따라 테두리 색이 바뀐다. (이기면 )
 
 const choice = {
   rock:{
@@ -25,17 +27,44 @@ const choice = {
 }
 
 function App() {
-  const [userSelect,setUserSelect] = useState(null)
-
+  const [userSelect,setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect]=useState(null);
+  const [result,setResult]=useState("");
   const play=(userChoice)=>{
-    setUserSelect(choice[userChoice])
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice],computerChoice));
   };
+
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Rock")
+      return computer.name === "Scissors" ? "win" : "lose";
+    else if (user.name === "Scissors")
+      return computer.name == "Paper" ? "win" : "lose";
+    else if (user.name === "Paper") return computer.name === "Rock" ? "win" : "lose";
+  };
+
+  const randomChoice=()=>{
+
+    let itemArray = Object.keys(choice);  //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
+    console.log("item array", itemArray);
+
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+
+    return choice[final];
+  }
 
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        {/* <Box title="Computer" /> */}
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect}/>
       </div>
       <div className="main">
       <button onClick={() => play("scissors")}>가위</button>
